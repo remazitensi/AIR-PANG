0cf285268e56682fc83c80a41a7d5e40629ae0ec
lsiron <l66612472@gmail.com>
2024-08-07 01:42:05 +0900

    refactor: App.jsx에 useEffect 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import MainPage from "./pages/MainPage";
