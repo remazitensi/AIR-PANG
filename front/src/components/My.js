9275b89f6b4bd1e4054ca123affd61b5b66af3c1
[AI11] 한정모 <hjm1044@naver.com>
2024-08-09 13:12:30 +0900

    fix: 랜딩페이지 수정

--- front/src/components/My.js
+++ front/src/components/My.js
@@ -9,6 +9,24 @@ const MyPage = () => {
   useEffect(() => {
     const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
     setFavorites(storedFavorites);
+
+    if (storedFavorites.length > 0) {
+      // 첫 번째 관심지역을 WeatherLanding에서 기본값으로 사용하도록 저장
+      localStorage.setItem(
+        "defaultLocation",
+        JSON.stringify(storedFavorites[0])
+      );
+    } else {
+      // 즐겨찾기 항목이 없을 경우 기본값으로 서울 강남구를 설정
+      localStorage.setItem(
+        "defaultLocation",
+        JSON.stringify({
+          id: 1,
+          address_a_name: "서울",
+          address_b_name: "강남구",
+        })
+      );
+    }
   }, []);
 
   const handleViewWeather = (location) => {
@@ -24,6 +42,24 @@ const MyPage = () => {
     const updatedFavorites = favorites.filter((fav) => fav.id !== id);
     setFavorites(updatedFavorites);
     localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+
+    if (updatedFavorites.length === 0) {
+      // 즐겨찾기 항목이 모두 삭제되면 서울 강남구를 기본값으로 설정
+      localStorage.setItem(
+        "defaultLocation",
+        JSON.stringify({
+          id: 1,
+          address_a_name: "서울",
+          address_b_name: "강남구",
+        })
+      );
+    } else {
+      // 첫 번째 관심지역을 WeatherLanding에서 기본값으로 사용하도록 저장
+      localStorage.setItem(
+        "defaultLocation",
+        JSON.stringify(updatedFavorites[0])
+      );
+    }
   };
 
   return (
